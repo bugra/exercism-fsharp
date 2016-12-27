a6c8215dcc060e8191d868bad4952ada9de444f7
bugra <bugraa@live.com>
2016-12-26 23:24:53 -0500

    SumOfMultiples program submission

---
+++ SumOfMultiples/SumOfMultiplesProgram.fs
@@ -0,0 +1,18 @@
+﻿module SumOfMultiplesProgram
+
+
+let isDivisibleBy dividend divisor =
+    divisor % dividend = 0
+
+// .. is inclusive in both boundaries
+let findDivisiblesBy number divisor =
+    [1 .. number-1]
+    |> List.filter (isDivisibleBy divisor)
+
+// List.collect is similar to flatMap in Scala
+let sumOfMultiples multiples number =
+    multiples
+    |> List.map (findDivisiblesBy number)
+    |> List.collect id
+    |> List.distinct
+    |> List.sum
