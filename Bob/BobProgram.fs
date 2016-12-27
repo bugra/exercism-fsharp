ca2d8d9d4f33e14d128cdf3b99c148b99ec71a09
bugra <bugraa@live.com>
2016-12-26 22:47:03 -0500

    Add Bob Program submission

---
+++ Bob/BobProgram.fs
@@ -0,0 +1,27 @@
+﻿module BobProgram 
+
+let isUpperCase input = 
+    input
+    |> String.filter (fun c -> System.Char.IsWhiteSpace(c) |> not)
+    |> String.filter (fun c -> System.Char.IsLetterOrDigit(c))
+    |> String.forall (fun c -> System.Char.IsUpper(c))
+
+let isQuestion (input: string) = 
+    (isUpperCase(input) |> not) && input.[input.Length - 1] = '?'
+
+let isShouting input = 
+    input 
+    |> isUpperCase
+
+let isExcited input = 
+    String.exists (fun c -> c = '!') input || isShouting input
+    
+let isWhiteSpace input = 
+    String.forall (fun c -> System.Char.IsWhiteSpace(c)) input
+
+let hey input =
+    match input with 
+    | input when isWhiteSpace input -> "Fine. Be that way!"
+    | input when isQuestion input -> "Sure."
+    | input when isExcited input -> "Whoa, chill out!"
+    | input -> "Whatever."
