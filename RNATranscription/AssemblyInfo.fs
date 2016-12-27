db8c2d52c6041f144b1ca12e6e2751a08978aea6
bugra <bugraa@live.com>
2016-12-26 23:19:41 -0500

    RNA Transcription submission is added

---
+++ RNATranscription/AssemblyInfo.fs
@@ -0,0 +1,20 @@
+﻿namespace RNATranscription
+open System.Reflection
+open System.Runtime.CompilerServices
+
+[<assembly: AssemblyTitle("RNATranscription")>]
+[<assembly: AssemblyDescription("")>]
+[<assembly: AssemblyConfiguration("")>]
+[<assembly: AssemblyCompany("Jet")>]
+[<assembly: AssemblyProduct("")>]
+[<assembly: AssemblyCopyright("")>]
+[<assembly: AssemblyTrademark("")>]
+
+// The assembly version has the format {Major}.{Minor}.{Build}.{Revision}
+
+[<assembly: AssemblyVersion("1.0.0.0")>]
+
+//[<assembly: AssemblyDelaySign(false)>]
+//[<assembly: AssemblyKeyFile("")>]
+
+()
---
+++ RNATranscription/RNATranscriptionProgram.fs
@@ -0,0 +1,14 @@
+﻿module RNATranscriptionProgram
+
+
+let transcription = function
+        | 'G' -> Some 'C'
+        | 'C' -> Some 'G'
+        | 'T' -> Some 'A'
+        | 'A' -> Some 'U'
+        | _   -> None
+
+let toRna input = 
+    input 
+    |> Seq.choose transcription
+    
