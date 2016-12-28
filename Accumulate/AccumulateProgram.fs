384a1303840c2bd52b0777a5c07229830cf44054
bugra <bugraa@live.com>
2016-12-27 23:43:34 -0500

    Add Accumulate program submission

---
+++ Accumulate/AccumulateProgram.fs
@@ -0,0 +1,10 @@
+﻿module AccumulateProgram
+
+
+let accumulate fn input = 
+    let rec accumulate' acc = function
+        | []    -> acc
+        | head::tail -> accumulate' (fn head :: acc) tail
+    
+    accumulate' [] input 
+    |> List.rev
