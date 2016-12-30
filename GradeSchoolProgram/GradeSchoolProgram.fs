1d887f9083f0f867f698f5ce6f46c613cb8310c5
bugra <bugraa@live.com>
2016-12-30 10:47:09 -0500

    Grade school program submission

---
+++ GradeSchoolProgram/GradeSchoolProgram.fs
@@ -0,0 +1,25 @@
+﻿module GradeSchoolProgram
+
+let mutable empty = Map.empty<string, int>
+
+let add x y (school: Map<string, int>) = 
+    school.Add(x, y)
+
+let getFirstItems aList =
+    aList
+    |> List.map fst
+
+
+let roster school=
+    school
+    |> Map.toList
+    |> List.groupBy snd
+    |> List.map (fun (x, y) -> (x, getFirstItems y))
+    |> List.sortBy fst
+
+
+let grade number school =
+    school
+    |> Map.toSeq
+    |> Seq.filter (fun (x, y) -> y = number)
+    |> Seq.map (fun (x, y) -> x)    
