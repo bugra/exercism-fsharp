f36f96f01146cf197345a5f9c86879d359403d63
bugra <bugraa@live.com>
2016-12-30 10:44:33 -0500

    ETL program submission

---
+++ ETLProgram/AssemblyInfo.fs
@@ -0,0 +1,20 @@
+﻿namespace ETL
+open System.Reflection
+open System.Runtime.CompilerServices
+
+[<assembly: AssemblyTitle("ETL")>]
+[<assembly: AssemblyDescription("")>]
+[<assembly: AssemblyConfiguration("")>]
+[<assembly: AssemblyCompany("Jet")>]
+[<assembly: AssemblyProduct("")>]
+[<assembly: AssemblyCopyright("")>]
+[<assembly: AssemblyTrademark("")>]
+
+// The assembly version has the format {Major}.{Minor}.{Build}.{Revision}
+
+[<assembly: AssemblyVersion("1.0.0.0")>]
+
+//[<assembly: AssemblyDelaySign(false)>]
+//[<assembly: AssemblyKeyFile("")>]
+
+()
---
+++ ETLProgram/ETLProgram.fs
@@ -0,0 +1,12 @@
+﻿module ETLProgram
+
+
+let transform input =
+    input
+    |> Map.toList
+    |> List.map (fun (cnt, y) -> 
+        match y with 
+        | [] -> []
+        | aNumberOfItems -> List.map (fun (item: string) -> (item.ToLower(), cnt)) aNumberOfItems)
+    |> List.collect id
+    |> Map.ofSeq    
