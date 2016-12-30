dc8bffbc84a44bb36611488b005f403a1b19944b
bugra <bugraa@live.com>
2016-12-30 10:45:08 -0500

    Pangram program submission

---
+++ Pangram/PangramProgram.fs
@@ -0,0 +1,10 @@
+﻿module PangramProgram
+
+let alphabet = ['a'..'z'] |> Set.ofList
+let getLettersAsASet (input: string) = 
+    input.ToLower() 
+    |> Set.ofSeq
+
+let isPangram (input: string) =
+    Set.isSubset alphabet (getLettersAsASet input)
+    
