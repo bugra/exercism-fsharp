2c8799122054449064619fc218d2142a8a74bf07
bugra <bugraa@live.com>
2016-12-30 11:39:02 -0500

    RobotName program submission is added

---
+++ Robot/RobotNameProgram.fs
@@ -0,0 +1,31 @@
+﻿module RobotNameProgram    
+
+let ALPHABET = ['a' .. 'z']
+
+let RNG = System.Random()
+
+type Robot = {
+    Name: string option
+}
+
+let generateName() = 
+  
+  let digits = 
+    [1 .. 3]
+    |> List.map (fun _ -> sprintf "%i" (RNG.Next(0, 9)))
+
+  let letters = 
+    [1 .. 2]
+    |> List.map (fun _ -> sprintf "%c" ALPHABET.[RNG.Next(0, ALPHABET.Length - 1)])
+  
+  letters @ digits
+  |> String.concat ""
+  
+let name robot =
+  robot.Name.Value
+
+let mkRobot() = 
+  {Robot.Name = Some (generateName ())}
+
+let reset robot =
+  {robot with Name=Some (generateName())}    
