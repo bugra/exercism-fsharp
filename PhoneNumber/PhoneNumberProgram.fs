292b115a973f767a55dd23eb43f104701775f947
bugra <bugraa@live.com>
2016-12-30 16:48:11 -0500

    PhoneNumber program submission is added

---
+++ PhoneNumber/PhoneNumberProgram.fs
@@ -0,0 +1,17 @@
+﻿module PhoneNumberProgram
+
+open System
+
+let digitize number =
+  number 
+  |> String.filter Char.IsDigit
+
+let parsePhoneNumber phoneNumber = 
+  match digitize phoneNumber with
+  | number when String.length number = 10 -> Some number
+  | number when String.length number = 11 ->
+      match [for c in number -> c] with
+      | head :: tail when head = '1' -> Some (tail |> Array.ofList |> System.String)
+      | _ -> None
+  | _ -> None
+  
