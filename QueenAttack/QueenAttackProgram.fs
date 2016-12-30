ee420ff708ff0394b231e5370b3defc661addb88
bugra <bugraa@live.com>
2016-12-30 14:47:05 -0500

    Queeen Attack program submission is added

---
+++ QueenAttack/QueenAttackProgram.fs
@@ -0,0 +1,8 @@
+﻿module QueenAttackProgram
+
+
+let canAttack firstTuple secondTuple = 
+  match firstTuple, secondTuple with 
+  | firstTuple, secondTuple when firstTuple = secondTuple -> 
+    failwith "Both pieces cannot be put into the same place"
+  | (x1, y1), (x2, y2) -> x1 = x2 || y1 = y2 || abs(x1-x2) = abs(y1-y2)
