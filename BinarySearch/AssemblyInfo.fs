65384f70e3f8c19e5caa7e8e36fb2d40b3a0a7bd
bugra <bugraa@live.com>
2016-12-31 14:12:15 -0500

    Binary search program submission is added

---
+++ BinarySearch/AssemblyInfo.fs
@@ -0,0 +1,20 @@
+﻿namespace BinarySearch
+open System.Reflection
+open System.Runtime.CompilerServices
+
+[<assembly: AssemblyTitle("BinarySearch")>]
+[<assembly: AssemblyDescription("")>]
+[<assembly: AssemblyConfiguration("")>]
+[<assembly: AssemblyCompany("Jet")>]
+[<assembly: AssemblyProduct("")>]
+[<assembly: AssemblyCopyright("")>]
+[<assembly: AssemblyTrademark("")>]
+
+// The assembly version has the format {Major}.{Minor}.{Build}.{Revision}
+
+[<assembly: AssemblyVersion("1.0.0.0")>]
+
+//[<assembly: AssemblyDelaySign(false)>]
+//[<assembly: AssemblyKeyFile("")>]
+
+()
---
+++ BinarySearch/BinarySearchProgram.fs
@@ -0,0 +1,20 @@
+﻿module BinarySearchProgram
+
+
+let addToOption number optional  =
+  match optional with 
+  | Some value -> Some (value + number)
+  | _ -> None
+  
+    
+let rec binarySearch anArray value = 
+  match anArray with
+  | [||] -> None
+  | _ ->
+    let half = (anArray.Length / 2) 
+    match value with 
+    | value when value = anArray.[half] -> Some half
+    | value when value < anArray.[half] -> binarySearch anArray.[0..half-1] value
+    | value -> 
+      let maxIndex = anArray.Length - 1
+      binarySearch anArray.[half+1..maxIndex] value |> addToOption (half + 1)
