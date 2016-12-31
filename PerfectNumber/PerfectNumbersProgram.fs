40ac44bd50e9c302c66f36e223cb4b73c2c45382
bugra <bugraa@live.com>
2016-12-30 20:15:00 -0500

    PerfecTNumbers program submission is added

---
+++ PerfectNumber/PerfectNumbersProgram.fs
@@ -0,0 +1,30 @@
+﻿module PerfectNumbersProgram
+
+type NumberType =
+  | Deficient
+  | Perfect
+  | Abundant
+
+
+let half number = 
+  match number % 2 with
+  | 1 -> (number + 1) / 2
+  | 0 -> number / 2
+  | _ -> number
+
+
+let factors number =
+  [1 .. (half number)]
+  |> List.filter (fun n -> number % n = 0)
+
+
+let sumOfFactors number = 
+  factors number
+  |> List.sum
+
+
+let classify number = 
+  match sumOfFactors number with 
+  | sum when sum = number -> NumberType.Perfect
+  | sum when sum > number -> NumberType.Abundant
+  | _ -> NumberType.Deficient
