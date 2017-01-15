0588615f09bc7f9a40158733cce0636fefc6f2a2
bugra <bugraa@live.com>
2017-01-15 13:16:36 -0500

    Anagram program submission is added

---
+++ Anagram/AnagramProgram.fs
@@ -0,0 +1,12 @@
+﻿module AnagramProgram
+
+
+let isAnagram (first: string) (second: string) =
+  (first.ToLowerInvariant() |> Seq.sort |> List.ofSeq) = (second.ToLowerInvariant() |> Seq.sort |> List.ofSeq) && (first.ToLowerInvariant() <> second.ToLowerInvariant()), second
+
+
+let anagrams words word = 
+  words
+  |> List.map (isAnagram word)
+  |> List.filter fst
+  |> List.map snd
---
+++ Series/SeriesProgram.fs
@@ -0,0 +1,5 @@
+﻿namespace Series
+
+type Class1() = 
+    member this.X = "F#"
+
