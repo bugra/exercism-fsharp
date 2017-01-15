4daab36c9a3081eb2ae873fcd41c20f6b61375de
bugra <bugraa@live.com>
2017-01-15 16:15:23 -0500

    Acronmy program submission is added

---
+++ Acronym/AcronymProgram.fs
@@ -0,0 +1,23 @@
+﻿module AcronymProgram
+
+open System.Text.RegularExpressions
+
+
+let getAcronym input = 
+  let letters = 
+    Regex.Split(input, "[^\w]") 
+    |> Seq.map (fun x -> x.ToUpperInvariant())
+    |> Seq.filter (fun x -> x.Length > 0)
+    |> Seq.map (fun x -> x.[0])
+    |> Array.ofSeq
+
+  new string(letters)
+
+
+let acronym = function
+  | "" -> ""
+  | input -> getAcronym input
+
+
+
+
