0626e87d50bb29fad2f25ba67226b35c2a5db9d5
bugra <bugraa@live.com>
2017-01-15 14:25:16 -0500

    Simple linked list program submission is added

---
+++ SimpleLinkedList/SimpleLinkedListProgram.fs
@@ -0,0 +1,56 @@
+﻿module SimpleLinkedListProgram
+
+type LinkedList<'a> = 
+  | Head of 'a * LinkedList<'a>
+  | Nil
+
+
+let nil = Nil
+
+
+let isNil = function 
+  | Nil -> true
+  | _ -> false
+
+
+let datum = function
+  | Head(item, _) -> item
+  | _ -> failwith "This is Nil, you cannot pass!"
+
+
+let create head tail =
+  Head(head, tail)
+
+
+let next = function
+  | Head(_, tail) -> tail
+  | _ -> failwith "This is Nil, there is no next!"
+
+
+let toList linkedList = 
+  let rec toList' acc linked = 
+    match linked with 
+    | Nil -> acc |> List.rev
+    | Head(item, tail) -> toList' (item::acc) tail
+  toList' [] linkedList
+
+
+let reverse linkedList = 
+  let rec rev' acc linked = 
+    match linked with
+    | Nil -> acc
+    | Head(head, tail) -> rev' (create head acc) tail
+  rev' Nil linkedList
+
+
+let fromList aList = 
+  let rec fromList' acc aList' =
+    match aList' with 
+    | [] -> acc |> reverse
+    | head::tail -> fromList' (create head acc) tail
+  fromList' Nil aList
+
+
+
+
+
