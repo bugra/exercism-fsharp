5dc9bfddc559fd85430f4b9a81bad27f79dce7de
bugra <bugraa@live.com>
2017-01-16 12:04:28 -0500

    Matrix program submission is added

--- Matrix/MatrixProgram.fs
+++ Matrix/MatrixProgram.fs
@@ -1,46 +1,27 @@
-﻿# Matrix
+﻿module MatrixProgram
 
-Write a program that, given a string representing a matrix of numbers, can return the rows and columns of that matrix.
 
-So given a string with embedded newlines like:
+let fromString (input: string) =
+  input.Split([|'\n'|])
+  |> Array.map (fun x -> x.Split([|' '|]))
+  |> Array.map (Array.map int)
 
-> 9 8 7  
-> 5 3 2  
-> 6 6 7  
 
-representing this matrix:
+let transpose matrix =
+    let rec transpose' acc (matrix':int list list) =
+      match matrix'.Head.Length with 
+      | 0 -> acc |> List.rev
+      | _ -> transpose' ([for row in matrix' -> row.Head]::acc) (List.map (fun row -> match row with [] -> [] | h::t -> t) matrix')
+    transpose' [] matrix
 
-```plain
-    0  1  2
-  |---------
-0 | 9  8  7
-1 | 5  3  2
-2 | 6  6  7
-```
 
-your code should be able to spit out:
+let rows anArray = 
+  anArray
 
-- A list of the rows, reading each row left-to-right while moving
-  top-to-bottom across the rows,
-- A list of the columns, reading each column top-to-bottom while moving
-  from left-to-right.
 
-The rows for our example matrix:
-
-- 9, 8, 7
-- 5, 3, 2
-- 6, 6, 7
-
-And its columns:
-
-- 9, 5, 6
-- 8, 3, 6
-- 7, 2, 7
-
-## Source
-
-Warmup to the `saddle-points` warmup. [http://jumpstartlab.com](http://jumpstartlab.com)
-
-## Submitting Incomplete Problems
-It's possible to submit an incomplete solution so you can see how others have completed the exercise.
-
+let cols anArray = 
+  Array.map List.ofArray anArray
+  |> List.ofArray
+  |> transpose 
+  |> List.map Array.ofList 
+  |> Array.ofList
