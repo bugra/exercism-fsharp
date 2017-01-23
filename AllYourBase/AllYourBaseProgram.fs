d9861a6cdf5fa60b2891fe80d12e9f08a0d36030
bugra <bugraa@live.com>
2017-01-23 18:36:39 -0500

    All your base program submission is added

--- AllYourBase/AllYourBaseProgram.fs
+++ AllYourBase/AllYourBaseProgram.fs
@@ -1,9 +1,23 @@
 ﻿module AllYourBaseProgram
 
 
+let getLargestPower (number: int) (newBase: int) = 
+  Seq.initInfinite (fun n -> n + 1)
+  |> Seq.takeWhile (fun index -> (float newBase) ** (float index) <= (float number))
+  |> Seq.max
+
+
 let computeBase number newBase = 
   // We need to come up with a plan to find out the largest exponent of a base
   // then do the conversion where the base case is base is larger than the number itself
+  let largestPower = getLargestPower number newBase
+  /// We need to consume the number through its exponent application
+  [largestPower .. -1 .. 1]
+  // TODO: Need to consume the number while we are passing the number into a list
+  // TODO: take a look at List.mapFold, there must be a function that does this job already
+  // TODO: ideally, this can be a recursive function but since we know the total number in a list
+  // TODO: it may not make a lot of sense to follow through on that one
+  (*
   let rec computeBase' number newBase acc = 
     match number with
     | number when number < newBase -> number :: acc
@@ -13,11 +27,7 @@ let computeBase number newBase =
 
   computeBase' number newBase [] |> List.rev
 
-
-let getLargestPower (number: int) (newBase: int) = 
-  Seq.initInfinite (fun n -> n + 1)
-  |> Seq.takeWhile (fun index -> (float newBase) ** (float index) <= (float number))
-  |> Seq.max
+  *)
 
 
 let rebase (inputBase: int) inputDigits outputBase =
